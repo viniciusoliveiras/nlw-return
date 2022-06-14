9a8ef94e9aab072ad60f6796376a8ecbc12a955c
viniciusoliveiras <vinitag190@gmail.com>
2022-06-14 20:44:49 -0300

    teste com injeção de CSS

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import styleInject from 'style-inject';
+
+styleInject('__STYLE_CSS__');
+export { Widget } from './components/Widget';
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
 
-// https://vitejs.dev/config/
+import { peerDependencies, dependencies } from './package.json';
+
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react(), dts()],
+  build: {
+    lib: {
+      entry: './src/index.ts',
+      name: 'nlwReturn',
+      formats: ['cjs', 'es'],
+      fileName: format => `index.${format}.js`,
+    },
+
+    rollupOptions: {
+      output: {
+        inlineDynamicImports: true,
+      },
+      external: [
+        ...Object.keys(dependencies),
+        ...Object.keys(peerDependencies),
+      ],
+    },
+    target: 'esnext',
+    sourcemap: true,
+  },
+
+  define: {
+    'process.env': {},
+  },
+});
